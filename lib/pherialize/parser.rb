efa79dd8865cecd4254d44b040cd16b4244d8709
Jamie Rumbelow <jamie@jamierumbelow.net>
2011-01-29 11:05:05 +0000

    Added the start of a parser

---
+++ lib/pherialize/parser.rb
@@ -0,0 +1,55 @@
+module Pherialize
+  
+  # Pherialize::Parser takes in a series of tokens and
+  # loops through them, building up the object represented
+  class Parser
+    def initialize(tokens)
+      @tokens = tokens
+      @position = 0
+    end
+    
+    # -----------
+    #   Token retrieval
+    # -----------
+    
+    def current
+      @tokens[@position]
+    end
+    
+    def next(offset = 0)
+      @tokens[@position + 1 + offset]
+    end
+    
+    def previous(offset = 0)
+      @tokens[@position - 1 - offset]
+    end
+    
+    # -----------
+    #   Token matching
+    # -----------
+    
+    def match(token_one, token_two = false)
+      if token_two == false
+        if current.type == token_one
+          @position += 1
+          true
+        else
+          false
+        end
+      else
+        if current.type == token_one
+          if self.next.type == token_two
+            @position += 1
+            true
+          else
+            false
+          end
+        else
+          false
+        end  
+      end
+    end
+    
+    
+  end
+end
